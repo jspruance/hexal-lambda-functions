38e0a1c5dd753e2dc36fa08d79501c37ae99d31f
Jonathan Spruance <jgspruance@gmail.com>
2019-06-08 13:17:16 -0700

    Adding lambdas

---
+++ hexalPut.js
@@ -0,0 +1,38 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+  const documentClient = new AWS.DynamoDB.DocumentClient();
+
+  let responseBody = "";
+  let statusCode = 0;
+
+  const { id, productname } = JSON.parse(event.body);
+
+  const params = {
+    TableName: "Products",
+    Item: {
+      id: id,
+      productname: productname
+    }
+  };
+
+  try {
+    const data = await documentClient.put(params).promise();
+    responseBody = JSON.stringify(data);
+    statusCode = 201;
+  } catch(err) {
+    responseBody = `Unable to put product: ${err}`;
+    statusCode = 403;
+  }
+
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: responseBody
+  };
+
+  return response;
+};
